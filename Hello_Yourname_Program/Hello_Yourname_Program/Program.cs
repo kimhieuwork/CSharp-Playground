6e764ea9ef2f7a6622a466a970054a337c886562
kimhieuwork <ngkimhieu14@gmail.com>
2025-08-28 15:12:00 +0700

    add new program

---
+++ Hello_Yourname_Program/Hello_Yourname_Program/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Nhap ten cua ban: ");
+string? s = Console.ReadLine();
+void Hello(string name)
+{
+    Console.WriteLine("Hello " + name);
+}
+if (string.IsNullOrEmpty(s))
+{
+    Hello(s);
+}
+else
+{
+    Hello(s);
+}
+
+
